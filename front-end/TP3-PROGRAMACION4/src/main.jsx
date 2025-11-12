0228e37f6a130dd002c98f7466cb569747356ee0
Ismael Malanga <genaromalanga@gmail.com>
2025-11-11 23:56:36 -0300

    modifique el main para poder agregar la ruta e importar el componente registro de usuario

--- front-end/TP3-PROGRAMACION4/src/main.jsx
+++ front-end/TP3-PROGRAMACION4/src/main.jsx
@@ -4,6 +4,9 @@ import "@picocss/pico";
 import "./index.css";
 import { Layout } from "./Layout.jsx";
 import { Home } from "./Home.jsx";
+import { RegistroUsuario } from "./Componentes/RegistroUsuario.jsx";
+
+
 import { AuthPage, AuthProvider } from "./Auth.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Pacientes } from "./Componentes/Pacientes.jsx";
@@ -19,6 +22,7 @@ import { CrearTurno } from "./Componentes/CrearTurno.jsx";
 import { ModificarTurno } from "./Componentes/ModificarTurno.jsx";
 import { DetallesTurno } from "./Componentes/DetallesTurno.jsx";
 
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <AuthProvider>
@@ -26,7 +30,8 @@ createRoot(document.getElementById("root")).render(
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<Home />} />
-
+                        <Route path="registro" element={<RegistroUsuario />} />
+                        
                         
                         <Route path="pacientes" element={<AuthPage><Pacientes /></AuthPage>} />
                         <Route path="pacientes/crear" element={<AuthPage><CrearPaciente /></AuthPage>} />
