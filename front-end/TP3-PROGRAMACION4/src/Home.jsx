3ed8b235729edf07e2abaf37ca555a917233dc34
Ismael Malanga <genaromalanga@gmail.com>
2025-11-11 23:54:17 -0300

    modifique home para agregar el registrarse

--- front-end/TP3-PROGRAMACION4/src/Home.jsx
+++ front-end/TP3-PROGRAMACION4/src/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "./Auth.jsx";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
     const { isAuthenticated, login, error } = useAuth();
@@ -11,30 +12,32 @@ export const Home = () => {
     };
 
     if (isAuthenticated) {
-        return <h2>Bienvenido al sistema de turnos </h2>;
+        return (
+            <section className="bienvenida">
+                <h2>Bienvenido al sistema de turnos</h2>
+                <p>
+                    Desde aquí podrás gestionar fácilmente los <b>pacientes</b>, <b>médicos</b> y <b>turnos</b>.<br />
+                    Utilizá el menú superior para acceder a cada sección.
+                </p>
+            </section>
+        );
     }
 
     return (
-        <article>
+        <article className="login-container">
             <h2>Iniciar sesión</h2>
             <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={cred.email}
-                    onChange={(e) => setCred({ ...cred, email: e.target.value })}
-                    required
+                <input type="email" placeholder="Email" value={cred.email} onChange={(e) => setCred({ ...cred, email: e.target.value })} required
                 />
-                <input
-                    type="password"
-                    placeholder="Contraseña"
-                    value={cred.password}
-                    onChange={(e) => setCred({ ...cred, password: e.target.value })}
-                    required
+                <input type="password" placeholder="Contraseña" value={cred.password} onChange={(e) => setCred({ ...cred, password: e.target.value })} required
                 />
                 <button>Entrar</button>
             </form>
-            {error && <p style={{ color: "red" }}>{error}</p>}
+            {error && <p className="error">{error}</p>}
+
+            <p className="registro-link">
+                ¿No tenés cuenta? <Link to="/registro">Registrate acá</Link>
+            </p>
         </article>
     );
 };
